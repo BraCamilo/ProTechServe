c9d9ce97b3a836b6ecc9e33e8a2d3ea71ad55a9f
BraCamilo <brayancalvo64@outlook.com>
2024-10-17 16:51:00 -0500

    Actualizacion de elementos de creasion he inicio se sesion

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,28 +1,30 @@
+const creacionDePerfil = document.getElementById('creacion-de-perfil')
+const inicioDeSesion = document.getElementById('inicio-de-sesion')
 let usuarios = []
 
 class usuario{
-    constructor(nombre, correo, contrasena){
+    constructor(nombre, correo, contrasena, tipo){
         this.nombre = nombre;
         this.correo = correo;
         this.contrasena = contrasena;
+        this.tipo = tipo;
     }
 }
-
+//funsiones creacion de usuario
 botonDeInicio.addEventListener('click', (event) => {
     event.preventDefault(); // Evitar que el formulario se envíe y la página se recargue
-
-    // Llamar a la función que crea el nuevo usuario
     nuevoUsuario();
 });
-
 function nuevoUsuario(){
     let name = document.getElementById('name').value;
     let emailUser = document.getElementById('email').value;
     let passwordUser = document.getElementById('password').value;
+    let tipo = document.getElementById('tipo-perfil').value;
 
-    let nuevo1 = new usuario(name, emailUser, passwordUser)
+    let nuevo1 = new usuario(name, emailUser, passwordUser, tipo)
 
     usuarios.push(nuevo1)
     console.log(usuarios)
     document.getElementById('formulario').reset();
 }
+//funsiones inisio de sesion
