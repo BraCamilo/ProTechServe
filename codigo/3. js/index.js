0964f5e9cde848cf06330d453f1519351eb85f88
BraCamilo <brayancalvo64@outlook.com>
2024-10-18 14:46:08 -0500

    Nueva actualizacion de index y otros

--- codigo/3. js/index.js
+++ codigo/3. js/index.js
@@ -1,7 +1,11 @@
+/* Variables globales */
 const creacionDePerfil = document.getElementById('creacion-de-perfil')
 const inicioDeSesion = document.getElementById('inicio-de-sesion')
+const indexPagina = document.getElementById('index')
 let usuarios = []
+/* Fin variables globales */
 
+/* Clases */
 class usuario{
     constructor(nombre, correo, contrasena, tipo){
         this.nombre = nombre;
@@ -10,12 +14,30 @@ class usuario{
         this.tipo = tipo;
     }
 }
-//funsiones creacion de usuario
+/* Fin de clases */
+
+/* Funciones globales */
+window.addEventListener('load', inicio)
+function atrasInicio(){
+    atras.addEventListener('click', inicio)
+}
+/* Fin de funsiones globales */
+function inicio(){
+    indexPagina.style.display = 'block'
+    creacionDePerfil.style.display = 'none'
+    inicioDeSesion.style.display = 'none'
+    botonesInicio.addEventListener('click', nuevoUsuario)
+    botonCrearCuenta.addEventListener('click', sesionUsuario)
+}
+
 botonDeInicio.addEventListener('click', (event) => {
     event.preventDefault(); // Evitar que el formulario se envíe y la página se recargue
     nuevoUsuario();
 });
 function nuevoUsuario(){
+    indexPagina.style.display = 'none'
+    creacionDePerfil.style.display = 'flex'
+
     let name = document.getElementById('name').value;
     let emailUser = document.getElementById('email').value;
     let passwordUser = document.getElementById('password').value;
@@ -26,5 +48,9 @@ function nuevoUsuario(){
     usuarios.push(nuevo1)
     console.log(usuarios)
     document.getElementById('formulario').reset();
+    atrasInicio()
 }
 //funsiones inisio de sesion
+function sesionUsuario(){
+
+}
